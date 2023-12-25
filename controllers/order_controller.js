d97e2080d462251f4cc3f2e82d168337155830f0
Safal Shrestha <computerstha@gmail.com>
2023-12-25 13:47:00 +0545

    Added log

--- controllers/order_controller.js
+++ controllers/order_controller.js
@@ -18,7 +18,7 @@ class Order{
                 throw new Error("Failed to place order")
             }
         } catch (error) {
-            // console.log(error);
+             console.log(error);
             res.status(500).json({
                 success:false,
                 error:error.message
